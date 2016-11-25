6c58e3a2e1848fee351ca35e75b16d5950e19a76
niimailtah <niimailtah@gmail.com>
2016-11-25 16:18:39 +0500

    Added some implementation of states.

--- python/euphoria.py
+++ python/euphoria.py
@@ -7,7 +7,7 @@ import configparser
 import random
 import logging
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 
 # ==================================================================================================================================================================
@@ -146,13 +146,33 @@ class Rules(Machine):
 # ==================================================================================================================================================================
 class Exchange(Machine):
     def __init__(self):
-        pass
+        self.states = [
+            {'name': 'Begin'},         #
+            {'name': 'End'},           #
+            {'name': 'Error'}          #
+        ]
+        self.transitions = [
+            {'trigger': 'step', 'source': 'Begin',      'dest': 'End'}
+        ]
+        Machine.__init__(self, states=self.states, transitions=self.transitions,
+                         initial='Begin', send_event=True)
+        self.answer = None
 
 
 # ==================================================================================================================================================================
 class War(Machine):
     def __init__(self):
-        pass
+        self.states = [
+            {'name': 'Begin'},         #
+            {'name': 'End'},           #
+            {'name': 'Error'}          #
+        ]
+        self.transitions = [
+            {'trigger': 'step', 'source': 'Begin',      'dest': 'End'}
+        ]
+        Machine.__init__(self, states=self.states, transitions=self.transitions,
+                         initial='Begin', send_event=True)
+        self.answer = None
 
 
 # ==================================================================================================================================================================
@@ -160,12 +180,13 @@ class Euphoria(Machine):
     def __init__(self):
         self.states = [
             {'name': 'Begin'},             #
-            {'name': 'TrunkPoint1'},       #
-            {'name': 'TrunkPoint2'},       #
-            {'name': 'TrunkPoint3'},       #
-            {'name': 'TrunkPoint4'},       #
-            {'name': 'TrunkPoint5'},       #
-            {'name': 'TrunkPoint6'},       #
+            {'name': 'Node1'},             # after VizirMessage before CaravanProfit
+            {'name': 'Node2'},             # after CaravanProfit before Crisis/Exchange
+            {'name': 'Node3'},             # after Crisis/Exchange before CaravanSupply
+            {'name': 'Node4'},             # after CaravanSupply before CaravanRob
+            {'name': 'Node5'},             # after CaravanRob before Metropolitan
+            {'name': 'Node6'},             # after Metropolitan before War
+            {'name': 'Node7'},             # after War before ...
             {'name': 'End'},               #
             {'name': 'Intro'},             #
             {'name': 'MainMenu'},          #
@@ -203,25 +224,29 @@ class Euphoria(Machine):
             {'trigger': 'step', 'source': 'MainMenu', 'dest': 'End', 'conditions': 'answerIsThree'},
             {'trigger': 'step', 'source': 'MainMenu', 'dest': 'NewGame', 'conditions': 'answerIsOne'},
             {'trigger': 'step', 'source': 'NewGame', 'dest': 'PrintState'},
-            {'trigger': 'step', 'source': 'PrintState', 'dest': 'VizirMessage', 'conditions': 'isNotFirstYear'},
-            {'trigger': 'step', 'source': 'PrintState', 'dest': 'TrunkPoint1'},
-            {'trigger': 'step', 'source': 'VizirMessage', 'dest': 'TrunkPoint1'},
-            {'trigger': 'step', 'source': 'TrunkPoint1', 'dest': 'CaravanProfit', 'conditions': 'is5YearCaravan'},
-            {'trigger': 'step', 'source': 'TrunkPoint1', 'dest': 'TrunkPoint2', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'CaravanProfit', 'dest': 'TrunkPoint2', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'TrunkPoint2', 'dest': 'Crisis', 'conditions': 'isCrisis'},
-            {'trigger': 'step', 'source': 'Crisis', 'dest': 'TrunkPoint3', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'TrunkPoint2', 'dest': 'Exchange', 'unless': 'isCrisis'},
-            {'trigger': 'step', 'source': 'Exchange', 'dest': 'TrunkPoint3', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'TrunkPoint3', 'dest': 'CaravanSupply', 'conditions': 'is0YearCaravan'},
-            {'trigger': 'step', 'source': 'TrunkPoint3', 'dest': 'TrunkPoint4', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'CaravanSupply', 'dest': 'TrunkPoint4', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'TrunkPoint4', 'dest': 'CaravanRob', 'unless': 'is0YearCaravan'},
-            {'trigger': 'step', 'source': 'TrunkPoint4', 'dest': 'TrunkPoint5', 'before': 'update_random'},
-            {'trigger': 'step', 'source': 'TrunkPoint5', 'dest': 'Metropolitan', 'conditions': 'isReceiveMetropolitan'},
-            {'trigger': 'step', 'source': 'TrunkPoint5', 'dest': 'War'},
-            {'trigger': 'step', 'source': 'War', 'dest': 'TrunkPoint6'},
-            {'trigger': 'step', 'source': 'TrunkPoint6', 'dest': 'End'}
+            {'trigger': 'step', 'source': 'PrintState', 'dest': 'VizirMessage', 'unless': 'isFirstYear'},
+            {'trigger': 'step', 'source': 'PrintState', 'dest': 'Node1'},
+            {'trigger': 'step', 'source': 'VizirMessage', 'dest': 'Node1'},
+            {'trigger': 'step', 'source': 'Node1', 'dest': 'CaravanProfit', 'conditions': 'is5YearCaravan'},
+            {'trigger': 'step', 'source': 'Node1', 'dest': 'Node2', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'CaravanProfit', 'dest': 'Node2', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node2', 'dest': 'Crisis', 'conditions': 'isCrisis'},
+            {'trigger': 'step', 'source': 'Crisis', 'dest': 'Node3', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node2', 'dest': 'Exchange', 'unless': 'isCrisis'},
+            {'trigger': 'step', 'source': 'Exchange', 'dest': 'Node3', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node3', 'dest': 'CaravanSupply', 'conditions': 'is0YearCaravan', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node3', 'dest': 'Node4', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'CaravanSupply', 'dest': 'Node4', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node4', 'dest': 'CaravanRob', 'unless': 'is0YearCaravan', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node4', 'dest': 'Node5', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'CaravanRob', 'dest': 'Node5', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node5', 'dest': 'Metropolitan', 'conditions': 'isReceiveMetropolitan'},
+            {'trigger': 'step', 'source': 'Node5', 'dest': 'Node6', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Metropolitan', 'dest': 'Node6', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node6', 'dest': 'War', 'conditions': 'isReasonWar'},
+            {'trigger': 'step', 'source': 'Node6', 'dest': 'Node7', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'War', 'dest': 'Node7', 'before': 'update_random'},
+            {'trigger': 'step', 'source': 'Node7', 'dest': 'End'}
         ]
         Machine.__init__(self, states=self.states, transitions=self.transitions,
                          initial='Begin', send_event=True)
@@ -269,8 +294,10 @@ class Euphoria(Machine):
         self.caravan_year = 0
         self.caravane_money = 0
         self.is_crisis = False
+        self.are_you_married = False
         self.crop_yield_level = None
         self.total_guard_maintenance = None
+        self.is_revolution = False
         # средние цены
         self.price_gold = 1000  # руб/кг
         self.price_land = 200   # руб/га
@@ -299,31 +326,172 @@ class Euphoria(Machine):
         # TODO: добавить больше информации
 
     def on_enter_CaravanProfit(self, event):
-        # TODO: показать доход каравана
-        pass
+        profit = self.caravane_money * 6
+        print('Вернулся Ваш караван! Получено прибыли на сумму {} руб.!'.format(profit))
+        self.money += self.caravane_money
+        print('║ Наличность, руб │ {:>10} ║'.format(self.money)) # for test
+        self.caravane_money = 0
+        self.caravan_year = 0
 
     def on_enter_Crisis(self, event):
-        # TODO: показать информацию о кризисе
-        pass
+        print('Международный кризис! Торговля невозможна!')
+        print('Вашему государству объявлена экономическая блокада!')
+        self.is_crisis = True
 
     def on_enter_Exchange(self, event):
         # TODO: обработать биржу
-        pass
+        print('Желаете торговать на бирже (y/n)?  ==> ')
+        exchange = Exchange()
+        while exchange.state != 'End':
+            exchange.step()
+        self.is_crisis = True
 
     def on_enter_CaravanSupply(self, event):
-        # TODO: добавить обработку
+        logger.info('on_enter_CaravanSupply: self.random_number = {}'.format(self.random_number))
+        if self.random_number < 25:
+            print('Заморский купец предлагает снарядить караван.\nВы согласны (y/n) ==> ')
+            # TODO: добавить обработку
+            print('В казне - {} руб, сколько на караван: '.format(self.money))
+            self.caravane_money = 0 # TDOD: сдклать запрос и проверку на деньги
+            print('Караван отправился за три-девять земель...')
+            self.money -= self.caravane_money
+
         pass
 
     def on_enter_CaravanRob(self, event):
-        # TODO: добавить обработку
-        pass
+        logger.info('on_enter_CaravanRob: self.random_number = {}'.format(self.random_number))
+        if self.random_number < 20:
+            logger.info('on_enter_CaravanRob: rob')
+            self.update_random(event)
+            if self.random_number < 5:
+                printf('Произошло ЧП! Ваш караван полностью разграблен бандитами!!!')
+                self.caravan_year = 0
+                self.caravane_money = 0
+            else:
+                self.update_random(event) # TODO: номер должен быть от 0 до 40
+                robbed_money = int(self.caravane_money * self.random_number / 100)
+                print('Внимание, ЧП! Ваш караван ограблен бандитами на сумму {} руб.!!!'.format(robbed_money))
+                self.caravane_money -= robbed_money
+
 
     def on_enter_Metropolitan(self, event):
         # TODO: добавить обработку
         pass
 
-    def on_enter_TrunkPoint5(self, event):
-        self.caravan_year += 1
+    def on_enter_Node5(self, event):
+        if self.caravan_year > 0:
+            self.caravan_year += 1
+
+    def on_enter_War(self, event):
+        war = War()
+        while war.state != 'End':
+            war.step()
+
+    def on_enter_VizirCatch(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_Inheritance(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_ChildBirthDay(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_Marriage(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_WifeDead(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_WifeIn(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_Sickness(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_GiveGrain(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_MakeTurn(self, event):
+        # TODO: добавить обработку
+        pass
+
+    def on_enter_NextYear(self, event):
+        # TODO: добавить обработку
+        print('Будете править в следующем году (y/n)?  ==> ')
+
+    def on_enter_NYParty(self, event):
+        # TODO: добавить обработку
+        printf('Будете устраивать Новогодний Бал (y/n)?  ==> ')
+
+    def on_enter_EndTurn(self, event):
+        # TODO: добавить обработку
+        self.current_year += 1
+
+    def on_enter_End(self, event):
+        # TODO: добавить обработку
+        self.on_enter_PrintState(event)
+        if self.is_revolution:
+            print('Голодающий народ ВОССТАЛ и свергнул нерадивого правителя!!!')
+            print('Поздравляю Вас, батенька, с РЕВОЛЮЦИЕЙ, ха-ха...\n')
+        else:
+            print('Ваше правление завершилось...')
+        # -------------------------------------------------------------------------
+        # -------------------------------------------------------------------------
+        points = 0
+        points += int(self.money / 1000)
+        points += int(self.gold * 2)
+        points += int(self.land / 5)
+        points += int(self.grain / 100)
+        points += int(self.peasant / 20)
+        points += int(self.guard / 10)
+        # points += build_xram * 200
+        points += self.current_year * 10
+        # -------------------------------------------------------------------------
+        print('╔═════════════════╤══════╤═══════════╗')
+        print('║    Название     │ Коэф │   Очки    ║')
+        print('╠═════════════════╪══════╪═══════════╣')
+        print('║ Наличность      │ /1000│{:>10} ║'.format(int(self.money / 1000)))
+        print('║ Золото          │    *2│{:>10} ║'.format(int(self.gold * 2)))
+        print('║ Земля           │    /5│{:>10} ║'.format(int(self.land / 5)))
+        print('║ Зерно           │  /100│{:>10} ║'.format(int(self.grain / 100)))
+        print('║ Крестьяне       │   /20│{:>10} ║'.format(int(self.peasant / 20)))
+        print('║ Гвардия         │   /10│{:>10} ║'.format(int(self.guard / 10)))
+        print('║ Время правления │   *10│{:>10} ║'.format(self.current_year * 10))
+        print('╠═════════════════╧══════╪═══════════╣')
+        print('║ Общая сумма            │{:>10} ║'.format(points))
+        print('╚════════════════════════╧═══════════╝')
+        # print('новые храмы - {}; '.format(), build_xram * 200)
+        # -------------------------------------------------------------------------
+        print('Ну что ж... Поздравляю с успешным (?) окончанием игры.')
+        if points > 0 and points <= 100:
+            print('P.S. Вам бы лучше гусей пасти... Вместо Ваших крестьян...')
+        elif points > 100 and points <= 300:
+            print('P.S. Для новичка - сойдет... Хотя, конечно, неважно...')
+        elif points > 300 and points <= 500:
+            print('P.S. Ну, это уже кое-что... Худо-бедно, да ладно...')
+        elif points > 500 and points <= 1000:
+            print('P.S. Ну вот, кое-что уже получается. Старайтесь дальше...')
+        elif points > 1000 and points <= 3000:
+            print('P.S. Неплохо, весьма неплохо... Уважаю...')
+        elif points > 3000 and points <= 5000:
+            print('P.S. Что ж, видно, играть Вы умеете... Весьма недурственно...')
+        elif points > 5000 and points <= 10000:
+            print('P.S. Круто, что говорить... Да Вы, батенька, профессионал...')
+        elif points > 10000 and points <= 100000:
+            print('P.S. Прости их, Господи... Ну Вы, блин, даете!!!')
+        elif points > 100000:
+            print('P.S. NO pity, NO mercy, NO REGRET!!!!!!!!!!')
+        else:
+            print('ERROR!!!')
+        print('-' * 80)
 
     # условия
     def answerIsOne(self, event):
@@ -344,9 +512,9 @@ class Euphoria(Machine):
             return True
         return False
 
-    def isNotFirstYear(self, event):
-        logger.info('isNotFirstYear: self.current_year = {}'.format(self.current_year))
-        if self.current_year != 1:
+    def isFirstYear(self, event):
+        logger.info('isFirstYear: self.current_year = {}'.format(self.current_year))
+        if self.current_year == 1:
             return True
         return False
 
@@ -375,6 +543,23 @@ class Euphoria(Machine):
         if self.random_number < 20:
             return True
         return False
+
+    def isReasonWar(self, event):
+        logger.info('isReasonWar: self.random_number = {}'.format(self.random_number))
+        # TODO: добавить условие, если Вы отказали жениться
+        # print('Разозленный отказом жениться на его дочке, соседний король начал ВОЙНУ!')
+        if self.guard < 100:
+            if self.random_number > self.guard:
+                print('Соседние короли, видя малочисленность Ваших войск, объявили Вам ВОЙНУ!')
+                return True
+        else:
+            if self.random_number < 30:
+                print('Есть возможность объявить войну одному из соседей.\nОбъявляете? (y/n) ==> ')
+                return False # for test
+        return True # for test
+
+    def areYouMarried(self, event):
+        return self.are_you_married
 
     # утилитки
     def update_random(self, event):
